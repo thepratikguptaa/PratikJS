b452d61484b6f4352012a55dea8475f6eada15a0
thepratikguptaa <pratikguptamain@gmail.com>
2024-12-18 00:57:36 +0530

    datatypes in js

---
+++ datatypes.js
@@ -0,0 +1,30 @@
+//  Primitive (call by value) datatypes:
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+
+// Reference (Non primitive) datatypes:
+// Array, Objects, Functions
+
+const heros = ["ironman", "venom", "spiderman"];
+let myObj = {
+    name: "Pratik",
+    age: 19,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof myFunction);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
