7f3cfbfc9c510053961ad4df7575ef6e4d5b955e
thepratikguptaa <pratikguptamain@gmail.com>
2024-12-17 23:34:38 +0530

    conversion of datatypes

---
+++ convert.js
@@ -0,0 +1,24 @@
+let score = "100"
+let score1 = null
+console.log(typeof score)
+console.log(typeof score1)
+console.log(score)
+console.log(score1)
+
+let valueInNumber = Number(score) 
+let valueInNumber1 = Number(score1)//converted from string to number
+console.log(typeof valueInNumber)
+console.log(typeof valueInNumber1)
+console.log(valueInNumber)
+console.log(valueInNumber1)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false; "Pratik" => true
---
+++ new.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version
+console.log("hello world")
+console.log("My name is Pratik")
+const myName = "Pratik"
+let myCollege = "SMIT"
+let mySkills = null
+let myHobbies = undefined
+
+/*
+prefer not to use var for variables
+*/
+
+console.table([myName, myCollege, mySkills])
+
+// alert("Yaayyyy!!!") // we are using nodejs, not browser... this works in the browser console
+
+console.log(typeof null)
+console.log(typeof undefined)
