d6549d5eec60b31a46a175013e749271e3974322
thepratikguptaa <pratikguptamain@gmail.com>
2024-12-18 16:22:30 +0530

    arrays in js

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
-const myArr = [1, 2, 3, 4, 5] // can also keep mix of datatypes in the same array
-const myArr2 = new Array(1, 2, 3, 4)
+// const myArr = [1, 2, 3, 4, 5] // can also keep mix of datatypes in the same array
+// const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr2)
 // console.log(myArr2[1])
 
@@ -20,10 +20,38 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 // console.log(myArr2)
 
-
 // slice, splice
-console.log(myArr2.slice(0, 2))
-console.log(myArr2)
+// console.log(myArr2.slice(0, 2))
+// console.log(myArr2)
 
-console.log(myArr2.splice(0, 2)) // manipulates the original array
-console.log(myArr2) // notice the difference
+// console.log(myArr2.splice(0, 2)) // manipulates the original array
+// console.log(myArr2) // notice the difference
+
+// const marvelHeroes = ["thor", "ironman", "spiderman"]
+// const dcHeroes = ["superman", "flash", "batman"]
+
+// const allHeroes = marvelHeroes.concat(dcHeroes)
+// console.log(allHeroes)
+
+// const allNewHeroes = [...marvelHeroes, ...dcHeroes] // spread operator
+// console.log(allNewHeroes) // same as concat
+
+// messedUpArr = [1, 2, 3, 4, [5, 6, 7], 8, 9, [10, 11, [12, 13, [14, 15, [16, 17, [18, 19, [20]]]]]]]
+// console.log(messedUpArr)
+// const flatMessedUpArr = messedUpArr.flat(Infinity) // flattens the array to infinite depth
+// console.log(flatMessedUpArr)
+
+// console.log(Array.isArray("Pratik")) // returns false
+// console.log(Array.from("Pratik"))
+// console.log(Array.from({name: "Pratik"})) // interesting
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+// let score4 = 400
+// console.log(Array.of(score1, score2, score3, score4))
+
+
+
+
+
