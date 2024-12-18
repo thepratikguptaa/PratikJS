71f9563775921e5336549f512d02354533103548
thepratikguptaa <pratikguptamain@gmail.com>
2024-12-18 12:18:45 +0530

    strings in js

---
+++ strings.js
@@ -0,0 +1,24 @@
+// let name = "Pratik"
+let myName = new String("Pratik Gupta") // another way of creating a string
+let age = 19
+// console.log(`My name is ${myName} and I am ${age} years old`)  // string interpolation
+
+// console.log(myName[0])
+// let length = myName.length
+// console.log(length)
+// console.log(myName.toUpperCase())
+// console.log(myName.toLowerCase())
+// console.log(myName) // original string remains same
+// console.log(myName.charAt(3))
+// console.log(myName.indexOf('t'))
+// console.log(myName.substring(0, 4)) // from 0 to 3rd index
+// console.log("Hello".repeat(5))
+// console.log(myName.slice(-length, 4))
+// const newstring = "     Pratik Gupta   "
+// console.log(newstring)
+// console.log(newstring.trim()) // removes leading and trailing spaces
+// const url = "https://google.com"
+// console.log(url.replace("https://", ""))
+// console.log(url.includes(".com"))
+const poem = "rain rain go away"
+console.log(poem.split(" "))
