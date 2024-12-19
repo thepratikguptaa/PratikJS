9b947c3d16377f769987e32bccd09ed112371eab
thepratikguptaa <pratikguptamain@gmail.com>
2024-12-20 00:01:27 +0530

    scopes in js

--- scopes.js
+++ scopes.js
@@ -11,3 +11,42 @@ if (true) {
 // console.log(`Outer Value of a (Global scope): ${a}`)
 // console.log(b)
 // console.log(c) // that is why var is not good... it does not consider block scope and global scope
+
+function one() {
+    const username = "Pratik"
+    function two() {
+        const website = "Google"
+        console.log(username)
+    }
+    // console.log(website)
+    two()
+}
+// one()
+
+
+if (true) {
+    const userName = "Pratik"
+    if (userName === "Pratik") {
+        const website = " Google"
+        // console.log(userName + website)
+    }
+    // console.log(website)
+}
+// console.log(userName)
+
+
+
+// ++++++++++++++++++++ INTERESTING ++++++++++++++++++++++++
+
+
+
+console.log(addOne(5))
+function addOne(num) {      
+    return num + 1        // can execute this function even when initialization is done after calling
+}
+console.log(addTwo(5))
+const addTwo = function (num) { // this time holding the function in a variable  
+    return num + 2        // CANNOT execute this function in the same way
+}
+
+
