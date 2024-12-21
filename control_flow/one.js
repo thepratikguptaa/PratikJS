5e64a166c5b20acb355f251cd99e6ebe3db70769
thepratikguptaa <pratikguptamain@gmail.com>
2024-12-21 14:44:24 +0530

    control flow in js

--- control_flow/one.js
+++ control_flow/one.js
@@ -1,12 +1,17 @@
 // if
 
-const isUserLoggedIn = true
-if (isUserLoggedIn) {
-    console.log("User is logged in")
-} else {
-    console.log("User is not logged in")
+// const isUserLoggedIn = true
+// if (isUserLoggedIn) {
+//     console.log("User is logged in")
+// } else {
+//     console.log("User is not logged in")
+// }
+
+const balance = 100
+// if (balance > 500) console.log("User can buy a new game")
+
+if (balance >= 1000) {
+    console.log("User can buy two games")
+} else if (balance < 500) {
+    console.log("User can buy only one game")
 }
-
-
-
-
