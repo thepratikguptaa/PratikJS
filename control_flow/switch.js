c655beefba245606922858fdbdf100476eac625e
thepratikguptaa <pratikguptamain@gmail.com>
2024-12-21 16:13:22 +0530

    control flow in js

---
+++ control_flow/switch.js
@@ -0,0 +1,37 @@
+// switch (key) {
+//     case value:
+         // code block
+//         break;
+//     case value:
+         // code block
+//         break;
+//     default:
+         // code block
+//         break;
+// }
+
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("February")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("April")
+        break;
+    default:
+        console.log("Not a month")
+        break;
+}
+
+
+
+
+
+
---
+++ control_flow/truthy_falsy.js
@@ -0,0 +1,43 @@
+// const userEmail = "pratik@gmail.com"
+
+// if (userEmail) {
+//     console.log("Got user email")
+// } else {
+//     console.log("Did not get user email")
+// }
+
+// const userEmail1 = ""
+// if (userEmail1) {
+//     console.log("Got user email")
+// } else {
+//     console.log("Did not get user email")
+// }
+
+// const userEmail2 = []
+// if (userEmail2) {
+//     console.log("Got user email")
+// } else {
+//     console.log("Did not get user email")
+// }
+
+// const emptyObject = {}
+// if (Object.keys(emptyObject).length === 0) {
+//     console.log("Empty object")
+// } else {
+//     console.log("Not empty object")
+// }
+
+// const emptyArray = []
+// if (emptyArray.length === 0) {
+//     console.log("Empty array")
+// } else {
+//     console.log("Not empty array")
+// }
+
+
+
+// falsy values // values that evaluate to false
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values // values that evaluate to true
+// true, "0", "false", " ", [], {}, function()
