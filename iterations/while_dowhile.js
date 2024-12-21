c3a9c20e09ace23a4e32202f783f32fb32f958bd
thepratikguptaa <pratikguptamain@gmail.com>
2024-12-21 23:42:57 +0530

    while and do while in js

---
+++ iterations/while_dowhile.js
@@ -0,0 +1,37 @@
+// while
+
+// while (condition) {
+     // code block to be executed
+// }
+
+// let i = 1;
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// let myArray = ["Flash", "Batman", "Superman"]
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(myArray[arr])
+//     arr++
+// }
+
+
+// do while
+
+// do {
+     // code block to be executed
+// } while (condition)
+
+// let score = 1
+// do {
+//     console.log(`Score is: ${score}`)
+//     score++
+// } while (score <= 10)
+
+let score = 20
+do {
+    console.log(`Score is: ${score}`) // will execute at least once whether the condition is true or false
+    score++
+} while (score <= 10)
