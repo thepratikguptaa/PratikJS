2237dd809cba1be599c0dff55058deca035d52c6
thepratikguptaa <pratikguptamain@gmail.com>
2024-12-22 15:51:44 +0530

    reduce method in js

---
+++ iterations_and_methods/dot_reduce.js
@@ -0,0 +1,37 @@
+// reduce
+
+const myNums = [1, 2, 3, 4, 5]
+
+// const sum = myNums.reduce( function(accumulator, currentValue)  {return accumulator + currentValue}, 0)
+
+// const sum = myNums.reduce( (accumulator, currentValue) =>  {return accumulator + currentValue}, 0) // same with arrow function
+
+// const sum1 = myNums.reduce( function(accumulator, currentValue)  {
+//     console.log (`accumulator: ${accumulator} currentValue: ${currentValue}`) // to understand the working
+//     return accumulator + currentValue}, 0)
+
+
+// console.log(sum)
+// console.log(sum1)
+
+const shoppingCart = [
+    {
+        itemName: "Shoes",
+        price: 1999
+    },
+    {
+        itemName: "Shirt",
+        price: 1079
+    },
+    {
+        itemName: "Trousers",
+        price: 1099
+    },
+    {
+        itemName: "Hoodie",
+        price: 1499
+    },
+]
+
+const totalCost = shoppingCart.reduce( (acc, item) => acc + item.price, 0)
+console.log(totalCost)
